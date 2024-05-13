54c840170c69e89acab06ddcff05fc6d94db362f
domfam-sunho <146902447+domfam-sunho@users.noreply.github.com>
2024-05-13 16:20:10 +0900

    industrial 완료

--- kr/contact/inquiry.php
+++ kr/contact/inquiry.php
@@ -21,106 +21,121 @@ include $_SERVER["DOCUMENT_ROOT"].$site_directory."/include/dtd.php";
 </script>
 <? include $_SERVER["DOCUMENT_ROOT"].$site_directory."/include/top.php"; ?>
 				<!-- 컨텐츠 내용 -->
-				<article class="inquiry-page">
-					<div class="area">
-						<article class="contact-form">
-							<!-- 문의 폼 시작 -->
-							<form action="./inquiry_ok.php" name="form" method="post" enctype="multipart/form-data">
-								<section class="bbs-write-con">
-									<article class="bbs-inquiry-agree-con">
-										<h3 class="agree-tit">개인정보처리방침</h3>
-										<div class="inquiry-agreement-con editor">
-										<?
-										$page_row = $db->object("cs_page", "where page_index='privacy'");
-
-										$content = $page_row->content;
-										$content = str_replace("<p>","",$content);
-										$content = str_replace("</p>","<br/>",$content);
-										$content = $tools->strHtml($content);
-										echo $content;
-										?>
+				<article class="product-page inquiry-page">
+						<div class="area">
+							<article class="contact-form">
+								<!-- 문의 폼 시작 -->
+								<form action="./inquiry_ok.php" name="form" method="post" enctype="multipart/form-data">
+									<section class="bbs-write-con">
+										<article class="bbs-inquiry-agree-con">
+											<p class="agree-tit">개인정보처리방침</p>
+											<div class="inquiry-agreement-con editor">
+												<p>개인정보처리방침</p>
+											</div>
+											<p class="agree-txt">
+												<input type="checkbox" id="agree1">
+													<label for="agree1">개인정보처리방침에 동의합니다.
+												</label>
+											</p>
+										</article>
+										<article class="bbs-write-tbl-box">
+											<p class="inquiry-essential-txt"><span class="essential-icon">*</span>표시는 필수 입력 항목입니다.</p>
+											<table class="bbs-write-tbl">
+												<caption>문의폼입니다.</caption>
+												<colgroup>
+													<col style="width:16%;">
+													<col>
+												</colgroup>
+												<tbody>
+													<tr>
+														<th scope="row"><span class="essential-icon">*</span>이름</th>
+														<td><input type="text" class="write-input" name="name" required="required"></td>
+													</tr>
+													<tr>
+														<th scope="row"><span class="essential-icon">*</span>이메일</th>
+														<td>
+															<fieldset class="email-fieldset">
+																<input type="text" class="write-input" name="email1" required="required">
+																<span class="hypen">@</span>
+																<input type="text" class="write-input" name="email2" readonly required="required">
+																<select name="email3" class="write-select" onChange="res();" required="required">
+																	<option value="b">메일계정선택</option>
+																	<option value="a">직접입력</option>
+																	<option value="naver.com">naver.com</option>
+																	<option value="nate.com">nate.com</option>
+																	<option value="hanmail.net">hanmail.net</option>
+																	<option value="gmail.com">gmail.com</option>
+																	<option value="hotmail.com">hotmail.com</option>
+																	<option value="outlook.com">outlook.com</option>
+																	<option value="empal.com">empal.com</option>
+																	<option value="dreamwiz.com">dreamwiz.com</option>
+																	<option value="lycos.co.kr">lycos.co.kr</option>
+																	<option value="yahoo.co.kr">yahoo.co.kr</option>
+																	<option value="korea.com">korea.com</option>
+																	<option value="paran.com">paran.com</option>
+																</select>
+															</fieldset>
+														</td>
+													</tr>
+													<tr>
+														<th scope="row"><span class="essential-icon">*</span>연락처</th>
+														<td>
+															<fieldset>
+																<input type="text" class="write-input" name="phone1" title="휴대폰번호 처음" maxlength="8" required="required">
+																<span class="hypen">-</span>
+																<input type="text" class="write-input" name="phone2" title="휴대폰번호 가운데" maxlength="8" required="required">
+																<span class="hypen">-</span>
+																<input type="text" class="write-input"  name="phone3" title="휴대폰번호 마지막" maxlength="8" required="required">
+															</fieldset>
+														</td>
+													</tr>
+													<tr>
+														<th scope="row"> 회사명</th>
+														<td><input type="text" class="write-input width-full" name="company"></td>
+													</tr>
+													<tr>
+														<th scope="row">부품명</th>
+														<td><input type="text" class="write-input width-full" name="part-name"></td>
+													</tr>
+													<tr>
+														<th scope="row">필요수량</th>
+														<td><input type="text" class="write-input width-full" name="request-quantity"></td>
+													</tr>
+													<tr>
+														<th scope="row">희망납기</th>
+														<td><input type="text" class="write-input width-full" name="wish-date"></td>
+													</tr>
+													<tr>
+														<th scope="row">목표단가</th>
+														<td><input type="text" class="write-input width-full" name="goal-price"></td>
+													</tr>
+													<tr>
+														<th scope="row">내용</th>
+														<td>
+															<textarea name="content" class="write-textarea" placeholder="문의내용 (500자 이내로 입력해주세요.)"></textarea>
+														</td>
+													</tr>
+												</tbody>
+											</table>
+											<dl class="contact">
+												<dt>* CONTACT:</dt>
+												<dd>이메일 <a href="mailto:info@microworks.co.kr">info@microworks.co.kr</a></dd>
+												<dd>문의전화 <a href="tel:02-6112-7328">02-6112-7328</a></dd>
+											</dl>
+										</article>
+										<!--
+											기본 : 센터정렬 / 좌측정렬 : cm-btn-align-left / 우측정렬 : cm-btn-align-right / 100% 버튼 : cm-btn-long-controls
+										-->
+										<div class="button-layout bottom-buttons">
+											<button type="button" class="button" onClick="sendit();"><strong>작성완료</strong></button>
+											<a href="/" class="button type-secondary"><strong>취소</strong></a>
 										</div>
-										<p class="agree-txt">
-											<input type="checkbox" id="agree1">
-												<label for="agree1">개인정보처리방침에 동의합니다.
-											</label>
-										</p>
-									</article>
-									<article class="bbs-write-tbl-box">
-										<p class="inquiry-essential-txt"><span class="essential-icon">*</span> 표시는 필수 입력 항목입니다.</p>
-										<table class="bbs-write-tbl">
-											<caption>문의폼입니다.</caption>
-											<colgroup>
-												<col style="width:20%;">
-												<col>
-											</colgroup>
-											<tbody>
-												<tr>
-													<th scope="row"><span class="essential-icon">*</span> 이름</th>
-													<td><input type="text" class="write-input" name="name"></td>
-												</tr>
-												<tr>
-													<th scope="row">이메일</th>
-													<td>
-														<fieldset class="email-fieldset">
-															<input type="text" class="write-input width20" name="email1">
-															<span class="hypen">@</span>
-															<input type="text" class="write-input width20" name="email2" readonly>
-															<select name="email3" class="write-select width20" onChange="res();">
-																<option value="b">메일계정선택</option>
-																<option value="a">직접입력</option>
-																<option value="naver.com">naver.com</option>
-																<option value="nate.com">nate.com</option>
-																<option value="hanmail.net">hanmail.net</option>
-																<option value="gmail.com">gmail.com</option>
-																<option value="hotmail.com">hotmail.com</option>
-																<option value="outlook.com">outlook.com</option>
-																<option value="empal.com">empal.com</option>
-																<option value="dreamwiz.com">dreamwiz.com</option>
-																<option value="lycos.co.kr">lycos.co.kr</option>
-																<option value="yahoo.co.kr">yahoo.co.kr</option>
-																<option value="korea.com">korea.com</option>
-																<option value="paran.com">paran.com</option>
-															</select>
-														</fieldset>
-													</td>
-												</tr>
-												<tr>
-													<th scope="row"><span class="essential-icon">*</span> 연락처</th>
-													<td class="tel-row">
-														<input type="text" class="write-input" style="max-width:150px;" name="phone1" title="휴대폰번호 처음" maxlength="8">
-														<span class="hypen">-</span>
-														<input type="text" class="write-input" style="max-width:150px;" name="phone2" title="휴대폰번호 가운데" maxlength="8">
-														<span class="hypen">-</span>
-														<input type="text" class="write-input" style="max-width:150px;"  name="phone3" title="휴대폰번호 마지막" maxlength="8">
-													</td>
-												</tr>
-												<tr>
-													<th scope="row"> 회사명</th>
-													<td><input type="text" class="write-input" name="company"></td>
-												</tr>
-												<tr>
-													<th scope="row">내용</th>
-													<td>
-														<textarea name="content" class="write-textarea"></textarea>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</article>
-									<!--  
-										기본 : 센터정렬 / 좌측정렬 : cm-btn-align-left / 우측정렬 : cm-btn-align-right / 100% 버튼 : cm-btn-long-controls
-									-->
-									<div class="cm-btn-controls">
-										<button type="button" class="btn-style01" onClick="sendit();">작성완료</button>
-										<a href="/" class="btn-style02">취소</a>
-									</div>
-								</section>
-							</form>
-							<!-- // 문의 폼 끝 -->
-						</article>
-					</div>
-				</article>
+									</section>
+								</form>
+								<!-- // 문의 폼 끝 -->
+							</article>
+						</div>
+					</article>
 				<!-- //컨텐츠 내용 -->
 <script type="text/javascript">
 <!--
---
+++ kr/industrial/innodisk.php
@@ -0,0 +1,272 @@
+<?
+$page_num = "04";
+$sub_num = "01";/* !NOTE : INNODISK 메뉴 추가 후 변경 해야함 */
+$page_section = "industrial";
+$sub_section = "innodisk";
+$page_info = "INDUSTRIAL";
+$sub_info = "INNODISK";
+include $_SERVER["DOCUMENT_ROOT"]."/lib/config.php";
+include "../lib/config.php";
+$sub_description = ""; // 페이지 설명(서브페이지) *필요시 사용
+include "../lib/sub.php";
+include $_SERVER["DOCUMENT_ROOT"].$site_directory."/include/dtd.php";
+?>
+<style>
+/* css */
+</style>
+<script>
+/* js */
+
+</script>
+<? include $_SERVER["DOCUMENT_ROOT"].$site_directory."/include/top.php"; ?>
+				<!-- 컨텐츠 내용 -->
+				<article class="industrial-top">
+						<div class="area">
+							<span class="logo"><img src="/data/bbsData/logo-innodisk.png" alt=""></span>
+							<article class="summary-page">
+								<article class="summary-top-box">
+									<div class="top-blue-box">
+										<div class="inner"></div>
+									</div>
+									<div class="top-txt-box">
+										<p class="txt">
+											Innodisk is a service-driven provider of flash memory, DRAM modules, embedded peripherals,
+											and software solutions for industrial and enterprise applications. With satisfied customers
+											across the embedded, server, in-vehicle, cloud storage markets, and more, we have set
+											ourselves apart from the competition with a commitment to dependable products and
+											unparalleled service quality. The result is solutions designed to supplement existing industrial
+											solutions and high IOPS flash arrays for industrial and enterprise applications. The expanded
+											business lines lead our next steps as a comprehensive solutions and service provider in the
+											industrial storage industry, and we look forward to working with our customers and partners,
+											jointly Building an Intelligent World.<br/>
+											<br/>
+											Founded in 2005 and headquartered in Taipei, Taiwan, Innodisk services clients globally with
+											engineering experts and sales teams in China, Japan, France, the Netherlands, and the United
+											States. With abundant experience and unrivaled knowledge of the memory industry, Innodisk
+											develops products with excellent quality, remarkable performance, and the highest reliability.<br/>
+											<br/>
+											For more information about Innodisk, please visit https://www.innodisk.com.
+										</p>
+									</div>
+								</article>
+						</div>
+					</article>
+					<article class="industrial-content area">
+						<article class="industrial-prd">
+							<div class="industrial-prd-con">
+								<h3 class="tit">Our Advantages</h3>
+								<ul class="advantages-list">
+									<li class="item">
+										<img src="/images/content/img-advantage-01.png" alt="">
+										<div class="text-wrap">
+											<p class="title">Technical Aptitude by Design</p>
+											<p class="desc">Our advantage lies in our portfolio of hardware, software, and firmware <br class="pc-only"/>technology and how we arrange these basic building blocks into innovations.</p>
+										</div>
+									</li>
+									<li class="item">
+										<img src="/images/content/img-advantage-02.png" alt="">
+										<div class="text-wrap">
+											<p class="title">Deeply Rooted in the Market</p>
+											<p class="desc">Our awareness of the pitfalls and opportunities in vertical markets allows us to <br class="pc-only"/>see the whole picture when crafting the optimal solution.</p>
+										</div>
+									</li>
+									<li class="item">
+										<img src="/images/content/img-advantage-03.png" alt="">
+										<div class="text-wrap">
+											<p class="title">We Are in It Together</p>
+											<p class="desc">To reach the optimal solution, working together with our partner from day one <br class="pc-only"/>is paramount. The best possible outcome is always managed by developing solutions jointly.</p>
+										</div>
+									</li>
+								</ul>
+							</div>
+							<div class="industrial-prd-con">
+								<h3 class="tit">Flash Storage</h3>
+								<div class="prd-sub-con">
+									<ul class="prd-list clearfix">
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_001.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_002.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_003.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_004.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_005.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_006.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_007.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_008.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_009.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_010.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_011.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_012.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_013.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_014.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_015.png" alt=""></span>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="industrial-prd-con">
+								<h3 class="tit">DRAM Modules</h3>
+								<div class="prd-sub-con">
+									<ul class="prd-list clearfix">
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_016.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_017.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_018.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_019.png" alt=""></span>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="industrial-prd-con">
+								<h3 class="tit">AloT Peripherals</h3>
+								<div class="prd-sub-con">
+									<ul class="prd-list clearfix">
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_020.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_021.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_022.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_023.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_024.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_025.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_026.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_027.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_028.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_029.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_030.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_031.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_032.png" alt=""></span>
+											</div>
+										</li>
+										<li>
+											<div class="inner">
+												<span class="prd-img"><img src="/data/goodsImages/goods1_033.png" alt=""></span>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</article>
+					</article>
+				<!-- //컨텐츠 내용 -->
+		
+<? include $_SERVER["DOCUMENT_ROOT"].$site_directory."/include/bottom.php"; ?>
