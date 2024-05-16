a6abcd8d8e904ca78f0ba10a63faf54f9c751042
domfam-sunho <146902447+domfam-sunho@users.noreply.github.com>
2024-05-16 14:14:09 +0900

    include 수정

--- cn/include/menu_product.php
+++ cn/include/menu_product.php
@@ -1,4 +1,14 @@
-								<li><a href="<?=$site_url?>/product/trend_list.php"><span><em>Memory 趋势</em></span></a></li>
+								<li><a href="<?=$site_url?>/product/trend_list.php"><span><em>Memory 趋势</em></span></a>
+									<!-- !NOTE S: 2024-04 추가 -->
+									<div class="gnb-3dep">
+										<ul>
+											<li><a href="<?=$site_url?>/product/trend_list.php">메모리 가격동향</a></li>
+											<li><a href="<?=$site_url?>/product/trend_list_2.php">메모리 대치품 검색</a></li>
+											<li><a href="<?=$site_url?>/product/trend_list_3.php">메모리 스탁재고 문의</a></li>
+										</ul>
+									</div>
+									<!-- !NOTE E: 2024-04 추가 -->
+								</li>
 								<li><a href="<?=$site_url?>/product/stock.php"><span><em>现货清单</em></span></a></li>
 								<li><a href="<?=$site_url?>/product/oem.php"><span><em>OEM Excess</em></span></a></li>
 								<!-- <li><a href="<?=$site_url?>/product/datasheet.php"><span><em>Datasheet</em></span></a></li> -->
